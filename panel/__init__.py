7cca71e25a61057821a498047d8e8660c40616fe
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-09 04:40:17 -0700

    platform: register hosting control APIs

--- panel/__init__.py
+++ panel/__init__.py
@@ -20,6 +20,7 @@ from .routes_dns import register_dns_routes
 from .routes_vault import register_vault_routes
 from .routes_integrations import register_integration_routes
 from .routes_remote_backup import register_remote_backup_routes
+from .routes_hosting import register_hosting_routes
 
 ROOT = Path(__file__).resolve().parents[1]
 
@@ -62,4 +63,5 @@ def create_app() -> Flask:
     register_vault_routes(app)
     register_integration_routes(app)
     register_remote_backup_routes(app)
+    register_hosting_routes(app)
     return app
