6532d1f7db1da5a6d40bb8746517ad46057f425e
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-12 06:42:59 -0700

    Register WordPress staging module

--- panel/module_registry.py
+++ panel/module_registry.py
@@ -11,6 +11,7 @@ from .domain_schema import ensure_domain_schema
 from .mail_schema import ensure_mail_schema
 from .ops_schema import ensure_ops_schema
 from .transfer_schema import ensure_transfer_schema
+from .wordpress_staging_schema import ensure_wordpress_staging_schema
 from .routes_accounts import register_account_routes
 from .routes_advanced_ops import register_advanced_ops_routes
 from .routes_auth import register_auth_routes
@@ -36,6 +37,7 @@ from .routes_transfers import register_transfer_routes
 from .routes_vault import register_vault_routes
 from .routes_webtools import register_webtools_routes
 from .routes_wordpress_lifecycle import register_wordpress_lifecycle_routes
+from .routes_wordpress_staging import register_wordpress_staging_routes
 from .routes_wordpress_updates import register_wordpress_update_routes
 
 SchemaHook = Callable[[], None]
@@ -84,6 +86,7 @@ MODULES: tuple[PanelModule, ...] = (
     PanelModule("domain_health", "Domain Readiness", "observability", register_domain_health_routes, depends_on=("domains", "advanced_ops", "autossl", "deliverability"), feature_prefixes=("domains.", "security.ssl", "email.deliverability"), ui_view="advancedops"),
     PanelModule("change_safety", "Change Safety", "observability", register_change_safety_routes, depends_on=("health", "advanced_ops", "domain_health"), ui_view="advancedops"),
     PanelModule("wordpress_lifecycle", "WordPress Lifecycle", "hosting", register_wordpress_lifecycle_routes, depends_on=("platform", "hosting"), feature_prefixes=("software.wordpress",), provider_services=("nexvary-panel-wordpress",), ui_view="wordpress", maturity="provider"),
+    PanelModule("wordpress_staging", "WordPress Staging", "hosting", register_wordpress_staging_routes, schema_hook=ensure_wordpress_staging_schema, depends_on=("wordpress_lifecycle", "sites"), feature_prefixes=("software.wordpress",), provider_services=("nexvary-panel-wordpress",), ui_view="wordpress", maturity="provider"),
     PanelModule("wordpress_updates", "WordPress Components", "hosting", register_wordpress_update_routes, depends_on=("wordpress_lifecycle",), feature_prefixes=("software.wordpress",), provider_services=("nexvary-panel-wordpress",), ui_view="wordpress", maturity="provider"),
 )
 
