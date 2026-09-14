3074f858096ad1b91f398ac319d87c326396d6b9
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-14 03:48:06 -0700

    Enable Track Delivery for default hosting accounts

--- panel/hosting_policy.py
+++ panel/hosting_policy.py
@@ -8,7 +8,7 @@ from .hosting_features import FEATURES
 DEFAULT_ACCOUNT_FEATURES = {
     "files.file_manager", "files.disk_usage", "files.ftp_accounts", "files.backups", "files.backup_wizard", "files.git",
     "domains.domains", "domains.redirects", "domains.zone_editor",
-    "email.accounts", "email.forwarders", "email.deliverability",
+    "email.accounts", "email.forwarders", "email.deliverability", "email.delivery_trace",
     "databases.mariadb", "databases.wizard",
     "metrics.visitors", "metrics.errors", "metrics.bandwidth", "metrics.resource_usage",
     "security.ssl_tls", "security.two_factor", "security.ssl_status",
